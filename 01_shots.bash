943d2fe684a81b0067983da4967a4acfd591a018
lncd <willforan@gmail.com>
2017-12-14 13:39:34 -0500

    freesurfer needs a subjectdir, use current directory

--- 01_shots.bash
+++ 01_shots.bash
@@ -1,3 +1,13 @@
 #!/usr/bin/env bash
+
+# start where this script is
+cd $(dirname $0)
+
+# where to save the screenshots
 [ ! -r imgs ] && mkdir imgs
+
+# use this fsaverage/ dir by cheating
+export SUBJECTS_DIR="$(pwd)"
+
+# take screenshots
 freeview -cmd shots_freeview_cmd.txt
