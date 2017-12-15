9b72874d35ca0ccc6ac2e06557f8f3e59cd870c3
lncd <willforan@gmail.com>
2017-12-15 15:58:15 -0500

    add beta

--- 00_makeme.bash
+++ 00_makeme.bash
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
+
+cd $(dirname $0)
 #
 # make freesurfer label annotations from SM input files
 # and copy locally
@@ -7,16 +9,29 @@
 
 # settings
 export SUBJECTS_DIR="/Volumes/T800/Multimodal/Rest_2015_97subjects/subjects"
-labelfile=20171214_nets
 
-# remove old
-for f in $SUBJECTS_DIR/fsaverage/label/{l,r}h.$labelfile.annot ; do
-  [ -r $f ] && rm $f
-done
+mkannot() {
+  dir="$1"
+  labelfile=20171214_nets_$dir
+  # remove old
+  for f in $SUBJECTS_DIR/fsaverage/label/{l,r}h.$labelfile.annot; do
+    [ -r $f ] && rm $f
+  done
+  
+  # create new
+  mris_label2annot --s fsaverage --h lh --ctab txt/$dir/networkLUT_lh.txt --a $labelfile $(seq 1 161 | xargs -I{} -n1 echo "--l $SUBJECTS_DIR/fsaverage/label/lh.label_{}.label")
+  mris_label2annot --s fsaverage --h rh --ctab txt/$dir/networkLUT_rh.txt --a $labelfile $(seq 162 333 | xargs -I{} -n1 echo "--l $SUBJECTS_DIR/fsaverage/label/rh.label_{}.label")
+  
+  # copy to local
+  cp $SUBJECTS_DIR/fsaverage/label/{l,r}h.$labelfile.annot  fsaverage/
+}
 
-# create new
-mris_label2annot --s fsaverage --h lh --ctab txt/networkLUT_lh.txt --a $labelfile $(seq 1 161 | xargs -I{} -n1 echo "--l $SUBJECTS_DIR/fsaverage/label/lh.label_{}.label")
-mris_label2annot --s fsaverage --h rh --ctab txt/networkLUT_rh.txt --a $labelfile $(seq 162 333 | xargs -I{} -n1 echo "--l $SUBJECTS_DIR/fsaverage/label/rh.label_{}.label")
-
-# copy to local
-cp $SUBJECTS_DIR/fsaverage/label/{l,r}h.$labelfile.annot  fsaverage/
+# do all or just the one specified
+if [ -z "$1" ]; then 
+  for d in txt/*/; do
+     mkannot $(basename $d)
+  done
+else
+  [ ! -d txt/$1 ] && echo "no LUT for '$1'" && exit 1
+  mkannot $1
+fi
--- 01_shots.bash
+++ 01_shots.bash
@@ -9,5 +9,39 @@ cd $(dirname $0)
 # use this fsaverage/ dir by cheating
 export SUBJECTS_DIR="$(pwd)"
 
+# make command file
+allsides(){
+  label=$1
+  hemi=$2
+  cat <<EOF
+     -cam Azimuth   0 -ss imgs/$label/${hemi}_0.png  2 
+     -cam Azimuth  90 -ss imgs/$label/${hemi}_90.png 2 
+     -cam Azimuth 180 -ss imgs/$label/${hemi}_180.png 2 
+     -cam Azimuth 270 -ss imgs/$label/${hemi}_270.png 2 
+EOF
+}
+
+fvcmd(){
+  label=$1
+  cat <<EOF
+   -f fsaverage/lh.inflated:annot=fsaverage/lh.20171214_nets_$label.annot 
+     $(allsides $label lh)
+   -unload surface
+   -f fsaverage/rh.inflated:annot=fsaverage/rh.20171214_nets_$label.annot 
+     $(allsides $label rh)
+   -f fsaverage/lh.inflated:annot=fsaverage/lh.20171214_nets_$label.annot 
+     $(allsides $label both)
+   -unload surface
+   -unload surface
+EOF
+}
+
+echo "-hide-3d-slices -nocursor -noquit" > shots_freeview_cmd.txt
+for luttxt in txt/*/; do
+ label=$(basename $luttxt)
+ fvcmd $label
+done >> shots_freeview_cmd.txt
+echo "-quit" >> shots_freeview_cmd.txt
+
 # take screenshots
 freeview -cmd shots_freeview_cmd.txt
